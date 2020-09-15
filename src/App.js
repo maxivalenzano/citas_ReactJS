d3e176bfe2f9008b13a7c46de038f7c9053236ed
maxivalenzano <maxivalenzano@gmail.com>
2020-09-15 19:40:19 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Formulario from './components/Formulario';
+import Cita from './components/Cita';
 
 function App() {
 
+  //Citas en local storage
+
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if (!citasIniciales) {
+    citasIniciales = [];
+  }
+
+
   //Arreglo de citas
-  const [citas, guardarCitas] = useState([]);
+  const [citas, guardarCitas] = useState(citasIniciales);
+
+  //UseEffect para realizar ciertas operaciones cuando el state cambia
+
+  useEffect ( () => {
+    if (citasIniciales) {
+      localStorage.setItem('citas', JSON.stringify(citas))
+    } else {
+      localStorage.setItem('citas', JSON.stringify([]))
+    };
+  }, [citas, citasIniciales] );
 
   //Funcion que tome las citas actuales y agregue las nuevas
   const crearCita = (cita) => {
     guardarCitas([
-      ...citas,
-      cita
-    ]);
+      ...citas, cita]);
   }
+
+  //Funcionar que elimina una cita por su id
+  const eliminarCita = (id) => {
+    const nuevasCitas = citas.filter(cita => cita.id !== id);
+    guardarCitas(nuevasCitas);
+  }
+
+  //Mensaje condicional 
+  const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus citas'
+
 
   return (
     <Fragment>
@@ -26,7 +53,14 @@ function App() {
             />
           </div>
           <div className="one-half column">
-            2
+            <h2>{titulo}</h2>
+            {citas.map(cita => (
+                <Cita
+                  key = {cita.id}
+                  cita= {cita}
+                  eliminarCita= {eliminarCita}
+                />
+            ))}
           </div>
         </div>
       </div>
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, {Fragment, useState} from 'react';
 import uuid from 'uuid/dist/v4';
+import PropTypes from 'prop-types';
 
 const Formulario = ({crearCita}) => {
 
@@ -113,5 +114,9 @@ const Formulario = ({crearCita}) => {
         </Fragment>
      );
 }
- 
+
+Formulario.propTypes = {
+    crearCita: PropTypes.func.isRequired
+}
+
 export default Formulario;
